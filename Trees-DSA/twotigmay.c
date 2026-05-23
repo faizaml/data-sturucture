905cf6c74f773a7f307481131352b47122b223b0
Faiz Amanullah <faizamnlh12@gmail.com>
2026-05-23 15:10:32 +0700

    update notes

--- Trees-DSA/twotigmay.c
+++ Trees-DSA/twotigmay.c
@@ -2,6 +2,18 @@
 #include <stdlib.h>
 #include <stdbool.h>
 
+/*
+--- Notes for tree ---
+1.pre order : pembacaan pre order ini dimulai dengan root - left - right . 
+pembacaan selalu mulai dari parent yang paling awal terlebih dahulu
+2. in order : pembacaan secara preorder ini selalu dimulai dari bagian leaf atau 
+dapat kita sebut juga dengan bagian left. langkah pembacaan ini dimulai dari left - root - right
+3. post order : pembacaan type ini dimulai dari cabang cabang nya terlbih dahulu, cabang pertama yang di baca atau di print itu adalah 
+cabang yang paling kiri terlebih dahulu, langkah pembacaan ini dimulai dari left - right - root
+*/
+
+
+/*--- Starting Code ---*/
 typedef struct Node
 {
     int data ;
@@ -17,6 +29,7 @@ Node* buatNode (int data){
     return nodeBaru ;
 }
 
+/* --- Easy Code --- */
 Node* tambahData (Node* root ,int data){
     if (root == NULL ){
         return buatNode(data) ;
@@ -35,10 +48,31 @@ Node* tambahData (Node* root ,int data){
 
 void inorder (Node* root){
     if (root == NULL)return ;
-    printf ("%d", root -> data) ;
-    inorder (root -> left) ;
-    inorder (root -> right) ;
+    inorder (root -> left) ; // kunjungin left
+    printf ("%d", root -> data) ; // kunjungin root
+    inorder (root -> right) ; // kunjungin right
 }
+
+void postorder 
+(Node* root){
+    if (root == NULL) return ;
+    postorder (root -> left) ;
+    postorder (root -> right) ;
+    printf ("%d", root -> data) ;
+}
+
+/* --- Mid Code --- */
+int countJumlah (Node* root){
+    if (root == NULL){
+        return 0 ;
+    }
+    
+    return 1 + countJumlah (root -> left) + countJumlah (root ->  right) ;
+}
+
+
+
+
 int main (){
     
     return 0 ;
