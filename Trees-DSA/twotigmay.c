9b30f7c7848eca42be1513c07d994a806a0e587b
faiaml <faizamnlh04@gmail.com>
2026-05-23 14:24:48 +0700

    updatecode

--- Trees-DSA/twotigmay.c
+++ Trees-DSA/twotigmay.c
@@ -1,5 +1,45 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+typedef struct Node
+{
+    int data ;
+    struct Node* left ; 
+    struct Node* right ;
+}Node;
+
+Node* buatNode (int data){
+    Node *nodeBaru = (Node*)malloc(sizeof(Node)) ;
+    nodeBaru -> data = data ;
+    nodeBaru ->  left = NULL ;
+    nodeBaru -> right = NULL ; 
+    return nodeBaru ;
+}
+
+Node* tambahData (Node* root ,int data){
+    if (root == NULL ){
+        return buatNode(data) ;
+    }
+    
+    if (data < root -> data){
+        root -> left = tambahData(root -> left , data) ;
+    }
+    
+    else if (data > root -> data){
+        root -> right = tambahData (root -> right, data) ;
+    }
+    
+    return root ;
+}
+
+void inorder (Node* root){
+    if (root == NULL)return ;
+    printf ("%d", root -> data) ;
+    inorder (root -> left) ;
+    inorder (root -> right) ;
+}
 int main (){
-    printf ("TEST GIT") ;
+    
     return 0 ;
 }
