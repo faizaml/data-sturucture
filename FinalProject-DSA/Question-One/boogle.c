0d817efd25a2126e12a1fc700380796726b42400
faizaml <faizamnlh04@gmail.com>
2026-05-25 15:39:09 +0700

    updatecode

--- FinalProject-DSA/Question-One/boogle.c
+++ FinalProject-DSA/Question-One/boogle.c
@@ -109,19 +109,137 @@ void viewAll(Trie* root) {
     getchar();
 }
 
-
-int main (){
-   
-    Trie* root = buatNode () ;
-
-    int menu ;
-    printf ("----- MENU -----\n") ;
-    printf ("1. Realease a new slang word\n") ;
-    printf ("View all slang words starting with a certain prefix word\n") ;
-    printf ("4. View all slang words\n") ;
-    printf ("5. Exit\n") ;
-    printf ("Choose menu : ") ;
-    scanf ("%d", &menu) ;
+void viewByPrefix(Trie* root) {
+    char prefix[100];
     
-    return 0 ;
+    printf("Input a prefix to be searched: ");
+    scanf("%s", prefix);
+    
+    Trie* nodePrefix = searchPref(root, prefix);
+    
+    if (nodePrefix == NULL) {
+        printf("There is no prefix \"%s\" in the dictionary.\n", prefix);
+    } else {
+        printf("Words starts with \"%s\":\n", prefix);
+        
+        char kataCari[1000];
+        strcpy(kataCari, prefix);
+        int panjangPrefix = strlen(prefix);
+        
+        cekKata(nodePrefix, kataCari, panjangPrefix);
+    }
+    
+    printf("\nPress enter to continue...");
+    getchar();
+    getchar();
+}
+
+void menuSearch(Trie* root) {
+    char kata[100];
+    
+    printf("Input a slang word to be searched: ");
+    scanf("%s", kata);
+    
+    char* hasil = search(root, kata);
+    
+    if (hasil == NULL) {
+        printf("There is no word \"%s\" in the dictionary.\n", kata);
+    } else {
+        printf("Slang word : %s\n", kata);
+        printf("Description : %s\n", hasil);
+    }
+    
+    printf("\nPress enter to continue...");
+    getchar();
+    getchar();
+}
+void menuRelease(Trie* root) {
+    char kata[100];
+    char deskripsi[1000];
+    
+    
+    while (1) {
+        printf("Input a new slang word [Must be >1 character & no space]: ");
+        scanf("%s", kata);
+        
+        if (strlen(kata) > 1) {
+            
+            break;
+        } else {
+            printf("Kata harus lebih dari 1 karakter!\n");
+        }
+    }
+    
+    while (1) {
+        printf("Input a new slang word description [Must be >2 words]: ");
+        getchar(); 
+        fgets(deskripsi, 1000, stdin);
+        deskripsi[strcspn(deskripsi, "\n")] = '\0'; 
+        
+        
+        int jumlahKata = 1;
+        for (int i = 0; i < strlen(deskripsi); i++) {
+            if (deskripsi[i] == ' ') jumlahKata++;
+        }
+        
+        if (jumlahKata > 2) {
+            break;
+        } else {
+            printf("Deskripsi harus lebih dari 2 kata!\n");
+        }
+    }
+    
+    insert(root, kata, deskripsi);
+    printf("Successfully released new slang word.\n");
+    
+    printf("\nPress enter to continue...");
+    getchar();
+    getchar();
+}
+
+
+int main() {
+    Trie* root = buatNode(); 
+    
+    int menu;
+    
+    do {
+        
+        printf("        BOOGLE SLANG WORD\n");
+
+        printf("1. Release a new slang word\n");
+        printf("2. Search a slang word\n");
+        printf("3. View all slang words starting with a certain prefix\n");
+        printf("4. View all slang words\n");
+        printf("5. Exit\n");
+        printf("--------------------------------\n");
+        printf("Choose menu: ");
+        scanf("%d", &menu);
+        
+        switch(menu) {
+            case 1:
+                menuRelease(root);
+                break;
+            case 2:
+                menuSearch(root);
+                break;
+            case 3:
+                viewByPrefix(root);
+                break;
+            case 4:
+                viewAll(root);
+                break;
+            case 5:
+                printf("Thank you... Have a nice day :)\n");
+                break;
+            default:
+                printf("Menu tidak valid!\n");
+                printf("Press enter to continue...");
+                getchar();
+                getchar();
+        }
+        
+    } while (menu != 5);
+    
+    return 0;
 }
