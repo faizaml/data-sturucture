0ea91a1e84ab89b870dfbb62410c5dd9d465c0c5
faizaml <faizamnlh04@gmail.com>
2026-05-26 10:53:47 +0700

    update code buat sesi dsa

---
+++ Disjoint - DSA/twosixdismay.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <stdlib.h>
+/*
+--- DISJOINT ---
+1. intinya dia tuh ga sama isinnya antar himpunan, kalau misal ada yang sama, mau itu satu biji atau lebih.itu gabisa, intinya dia itu gaboleh beririsan */
+int main (){
+
+    return 0 ;
+}
