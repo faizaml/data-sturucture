d86b3d1ebed8870843eb23a5713c0abcd87b5290
faizaml <faizamnlh04@gmail.com>
2026-05-27 17:47:42 +0700

    tambah file oj

---
+++ Online-Judge/orpbsi.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct Node
+{
+    int data ;
+    struct Node* next ;
+    struct Node* prev ;
+}Node;
+
+
+
+
+
+int main  (){
+
+    return 0 ;
+}
